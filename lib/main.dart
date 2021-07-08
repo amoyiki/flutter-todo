d6bac7fe1ef30053d17e38c9f84161a0a0b46111
guoyihui <249586923@qq.com>
2021-07-08 17:56:15 +0800

    get data from sqlfliet

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/sqlite_util.dart';
 import 'package:intl/intl.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'models.dart';
 
@@ -24,11 +24,13 @@ class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   TextEditingController _editingController = TextEditingController();
+  var _datas = <TaskModel>[];
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    // _retrieveData(1, 0);
   }
 
   @override
@@ -58,54 +60,7 @@ class _MainPageState extends State<MainPage>
           size: 40,
         ),
         onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: Text('新增事项'),
-                  content: SizedBox(
-                    height: 200,
-                    child: Card(
-                      elevation: 0.0,
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            keyboardType: TextInputType.multiline,
-                            autofocus: true,
-                            controller: _editingController,
-                            maxLines: null, //不限制行数
-                            decoration: InputDecoration(
-                              hintText: '输入待办事项',
-                              filled: true,
-                              fillColor: Colors.grey.shade50,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  actions: <Widget>[
-                    TextButton(
-                      child: Text('取消'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    TextButton(
-                      child: Text('确定'),
-                      onPressed: () async {
-                        DateTime now = DateTime.now();
-                        String currentStr = _editingController.text;
-                        DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss "); // how you want it to be formatted
-                        String nowStr = dateFormat.format(now); 
-                        TaskModel taskModel = TaskModel.fromJson({"content": currentStr, "isComplete": false, "created": nowStr, "updated": nowStr});
-                        await SqliteUtil.sqliteUtil.insertNewTask(taskModel);
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                );
-              });
+          createDialog();
         },
         backgroundColor: Colors.blue,
       ),
@@ -113,8 +68,18 @@ class _MainPageState extends State<MainPage>
       body: TabBarView(
         controller: _tabController,
         children: <Widget>[
-          Center(
-            child: Text("这是未完成列表"),
+          ListView.separated(
+            itemCount: _datas.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(16.0),
+                child: Text('====已经到达底线了==='),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+              height: .0,
+            ),
           ),
           Center(
             child: Text("这是已完成列表"),
@@ -122,5 +87,72 @@ class _MainPageState extends State<MainPage>
         ],
       ),
     );
+  }
+
+  /// 弹框方法
+  void createDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('新增事项'),
+            content: SizedBox(
+              height: 200,
+              child: Card(
+                elevation: 0.0,
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      autofocus: true,
+                      controller: _editingController,
+                      maxLines: null, //不限制行数
+                      decoration: InputDecoration(
+                        hintText: '输入待办事项',
+                        filled: true,
+                        fillColor: Colors.grey.shade50,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('取消'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                child: Text('确定'),
+                onPressed: () async {
+                  DateTime now = DateTime.now();
+                  String currentStr = _editingController.text;
+                  DateFormat dateFormat = DateFormat(
+                      "yyyy-MM-dd HH:mm:ss "); // how you want it to be formatted
+                  String nowStr = dateFormat.format(now);
+                  TaskModel taskModel = TaskModel.fromJson({
+                    "content": currentStr,
+                    "isComplete": false,
+                    "created": nowStr,
+                    "updated": nowStr
+                  });
+                  await SqliteUtil.sqliteUtil.insertNewTask(taskModel);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  void _retrieveData(int limit, int offset) {
+    LinkedHashMap map = new LinkedHashMap();
+    map['isComplete'] = false;
+    var taskByMap = SqliteUtil.sqliteUtil.getTaskByMap(map, limit, offset);
+    for (var m in taskByMap) {
+      _datas.add(TaskModel.fromJson(m));
+    }
   }
 }
--- lib/sqlite_util.dart
+++ lib/sqlite_util.dart
@@ -1,5 +1,4 @@
-
-
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:flutter_todo/constant.dart';
@@ -11,15 +10,14 @@ class SqliteUtil {
   SqliteUtil._();
   static SqliteUtil sqliteUtil = SqliteUtil._();
   static late Database _database;
-  Future<Database> get database async =>
-      _database = await createDatabase();
+  Future<Database> get database async => _database = await createDatabase();
 
   Future<Database> createDatabase() async {
-
     Directory directory = await getApplicationDocumentsDirectory();
     String path = directory.path + 'todo.db';
     Database database = await openDatabase(path, version: 1, onCreate: (db, v) {
-      db.execute('CREATE TABLE $tableName($tableId INTEGER PRIMARY KEY AUTOINCREMENT, $tableContent TEXT, $tableIsComplete INTEGER, $tableCreated TEXT, $tableUpdated TEXT)');
+      db.execute(
+          'CREATE TABLE $tableName($tableId INTEGER PRIMARY KEY AUTOINCREMENT, $tableContent TEXT, $tableIsComplete INTEGER, $tableCreated TEXT, $tableUpdated TEXT)');
     });
     return database;
   }
@@ -28,7 +26,8 @@ class SqliteUtil {
     Database database = await sqliteUtil.database;
     int rowIndex = await database.insert(tableName, taskModel.toJson());
     print("add row $rowIndex");
-    List<Map> result = await database.query(tableName, where: "id = ?", whereArgs: [rowIndex]);
+    List<Map> result =
+        await database.query(tableName, where: "id = ?", whereArgs: [rowIndex]);
     print('data is ${result}');
   }
 
@@ -38,24 +37,59 @@ class SqliteUtil {
     print(result);
   }
 
+  getTaskByMap(LinkedHashMap where, int limit, int offset) async {
+    Database database = await sqliteUtil.database;
+    String whereStr = "";
+    List<String> whereKeyList = [];
+    List<String> whereValList = [];
+    for (var key in where.keys) {
+      whereKeyList.add(' $key = ? ');
+    }
+    for (var v in where.values) {
+      whereValList.add(v);
+    }
+    whereStr = whereKeyList.join("AND");
+    List<Map> result = await database.query(tableName,
+        where: whereStr, whereArgs: whereValList, limit: limit, offset: offset);
+    print(result);
+    return result;
+  }
+  getTaskCountByMap(LinkedHashMap where) async {
+    Database database = await sqliteUtil.database;
+    String whereStr = "";
+    List<String> whereKeyList = [];
+    List<String> whereValList = [];
+    for (var key in where.keys) {
+      whereKeyList.add(' $key = ? ');
+    }
+    for (var v in where.values) {
+      whereValList.add(v);
+    }
+    whereStr = whereKeyList.join("AND");
+    List<Map> result = await database.query(tableName, columns: ['id'],
+        where: whereStr, whereArgs: whereValList);
+    print(result);
+    return result.length;
+  }
+
   getTaskById(int id) async {
     Database database = await sqliteUtil.database;
-    List<Map> result = await database.query(tableName, where: "id = ?", whereArgs: [id]);
+    List<Map> result =
+        await database.query(tableName, where: "id = ?", whereArgs: [id]);
     print(result);
+    return result;
   }
 
   deleteTaskById(int id) async {
     Database database = await sqliteUtil.database;
-    int rowIndex = await database.delete(tableName, where: "id = ?", whereArgs: [id]);
+    int rowIndex =
+        await database.delete(tableName, where: "id = ?", whereArgs: [id]);
     print(rowIndex);
   }
 
   updateTask(TaskModel taskModel) async {
     Database database = await sqliteUtil.database;
-    database.update(tableName, taskModel.toJson(), where: "id = ?", whereArgs: [taskModel.id]);
+    database.update(tableName, taskModel.toJson(),
+        where: "id = ?", whereArgs: [taskModel.id]);
   }
-
-
-
-
 }
